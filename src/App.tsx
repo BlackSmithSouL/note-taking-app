cafa5da52aca5cfa506fdc19a53c3ad6e9b694aa
BlackSmithSouL <kowalskimarcinkontakt@gmail.com>
2023-03-04 14:00:27 +0100

    Create note edit page

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { v4 as uuidV4 } from "uuid"
 import { NoteList } from "./NoteList"
 import { NoteLayout } from "./NoteLayout"
 import { Note } from "./Note"
+import { EditNote } from "./EditNote"
 
 export type Note = {
   id: string
@@ -50,6 +51,18 @@ export const App = () => {
     })
   }
 
+  const onUpdateNote = (id: string, { tags, ...data}: NoteData) => {
+    setNotes(prevNotes => {
+      return prevNotes.map(note => {
+        if (note.id === id) {
+          return { ...note, ...data, tagIds: tags.map(tag => tag.id)}
+        } else {
+          return note
+        }
+      })
+    })
+  }
+
   const addTag = (tag: Tag) => {
     setTags(prev => [...prev, tag])
   }
@@ -67,7 +80,15 @@ export const App = () => {
       />} />
       <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
         <Route index element={<Note />} />
-        <Route path="edit" element={<h1>Edit</h1>} />
+        <Route 
+          path="edit" 
+          element={
+            <EditNote 
+              onSubmit={onUpdateNote} 
+              onAddTag={addTag} 
+              availableTags={tags}
+            />
+        } />
       </Route>
       <Route path="*" element={<Navigate to="/"/>} />
     </Routes>
--- src/Note.tsx
+++ src/Note.tsx
@@ -1,4 +1,5 @@
 import { Badge, Button, Col, Row, Stack } from "react-bootstrap"
+import { ReactMarkdown } from "react-markdown/lib/react-markdown"
 import { Link } from "react-router-dom"
 import { useNote } from "./NoteLayout"
 
@@ -38,5 +39,6 @@ export const Note = () => {
                 </Stack>
             </Col>
         </Row>
+        <ReactMarkdown>{note.markdown}</ReactMarkdown>
     </>
 }
